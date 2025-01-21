543932d8b1695d6d42f27d03739eb0fb5fd2ce57
diegolenci <lencidiego0@gmail.com>
2025-01-21 17:40:22 +0100

    add: compare il form e cambiano i testi allo scadere del timer

--- js/script.js
+++ js/script.js
@@ -1,29 +1,37 @@
+//GENERAZIONE 5 NUMERI RANDOM
+const numberList = document.getElementById('numbers-list')
 
-const divHtml = document.getElementById('random')
 
-for (var i = 0; i < 5; i++) {
-    var count = 0;
-    for (var j = 0; j < Math.floor(Math.random() * 100) + 1; j++) {
+for (let i = 0; i < 5; i++) {
+    let count = 0;
+    for (let j = 0; j < Math.floor(Math.random() * 100) + 1; j++) {
         count++;
     }
     console.log(count);
-
+    numberList.innerHTML = count
 }
-divHtml.innerHTML = count
 
 
 
 
+//TIMER DI 30 SECONDI
 let counter = 30
 const countdown = document.getElementById('countdown')
 const form = document.getElementById('answers-form')
+const instructions = document.getElementById('instructions')
 
 const inserisciNumeri = setInterval(gioca, 1000)
 function gioca(){
-
+    
+    //ALLO SCADERE: COMPARE IL FORM, CAMBIANO I TESTI
     if( counter === 0){
         clearInterval(inserisciNumeri)
         form.classList.remove('d-none')
+        countdown.innerHTML = "Inserisci i numeri"
+        instructions.classList.add('d-none')
+        numberList.innerHTML = "Vai!"
+
+
     } else{
         countdown.innerHTML = counter--
     }
@@ -31,4 +39,10 @@ function gioca(){
     return counter
 
 }
+
+
+const bottone = document.getElementsByClassName('btn')
+
+
+
 
